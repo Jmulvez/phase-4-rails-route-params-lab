16ff37e20d5b475c1c6f21e536818f03ce31bef2
John <johnmulvey48@yahoo.com>
2023-01-09 10:47:38 -0500

    git@github.com:Jmulvez/phase-4-rails-route-params-lab.git

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get '/students', to: 'students#index'
-  get '/students/grades', to: 'students#grades'
-  get '/students/highest-grade', to: 'students#highest_grade'
+  resources :students, only: [:index, :show]
 end
